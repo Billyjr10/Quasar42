71cb4b24659f05ff597b3e7023c2fc85e8875d30
unknown <billy2000guye@gmail.com>
2022-06-06 17:09:45 +0200

    fixing bugs

--- MyAppQuasar42/models.py
+++ MyAppQuasar42/models.py
@@ -1,11 +1,5 @@
 
-import email
-from email import message
-from logging import PlaceHolder
-from pyexpat import model
-from tkinter import CASCADE
 
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.forms import CharField, HiddenInput, SplitDateTimeWidget, SplitHiddenDateTimeWidget
 
